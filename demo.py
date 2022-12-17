e9650da25992e64177e6442efbfe54f331cb9d71
imdemogithub <imdemoemail@gmail.com>
2022-12-17 12:06:25 +0530

    python data

---
+++ demo.py
@@ -0,0 +1,14 @@
+import my_codes
+
+print(dir(my_codes))
+
+print(type(my_codes.add))
+
+a = my_codes.add(5, 2)
+
+print(a)
+
+import random
+
+print(random.randrange(1, 10))
+print(random.randint(1, 10))
---
+++ my_codes.py
@@ -0,0 +1,5 @@
+'''just for demo purpose'''
+
+def add(x, y):
+    '''this is an addtion method. It will returns the total of x + y'''
+    return x + y
